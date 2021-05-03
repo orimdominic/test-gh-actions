a292f01555c940be6fb02408d13dc6d3a92ead82
Orim Dominic <orimdominic@gmail.com>
2021-05-03 05:33:36 +0100

    test gh action on pr

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,13 @@
-import { add } from "./index";
+import { add, concatStr } from "./index";
 
 describe("add", () => {
   test("it returns the sum of two integers", () => {
     expect(add(1, 3)).toBe(4);
+  });
+});
+
+describe("concatStr", () => {
+  test("it returns the concatenation of two strings", () => {
+    expect(concatStr("hello", "world")).toBe("helloworld");
   });
 });
