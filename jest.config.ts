04dd99b18454bd1d8c18d0a1ed9e925d30a90885
Orim Dominic <orimdominic@gmail.com>
2021-05-03 05:26:09 +0100

    fix eslint config and prettier format

--- jest.config.ts
+++ jest.config.ts
@@ -71,14 +71,7 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "jsx",
-    "ts",
-    "tsx",
-    "node"
-  ],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -152,9 +145,7 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  testPathIgnorePatterns: ["/node_modules/"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -172,7 +163,7 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-   transform: {
+  transform: {
     "^.+\\.ts$": "ts-jest",
   },
 
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
-import {add} from "./index";
+import { add } from "./index";
 
 describe("add", () => {
-	test("it returns the sum of two integers", () => {
-		expect(add(1,3)).toBe(4)
-	})
-})
+  test("it returns the sum of two integers", () => {
+    expect(add(1, 3)).toBe(4);
+  });
+});
